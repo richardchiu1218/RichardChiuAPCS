74f6da995284026b7bddf2375b39c139b3c5694a
richardchiu1218 <142924218+richardchiu1218@users.noreply.github.com>
2024-04-17 21:15:23 -0700

    Add files via upload

---
+++ CREATE 4:17:2024 Update/CREATE/rankings.js
@@ -0,0 +1,10 @@
+function findFighter() {
+    var weightClass = document.getElementById("weight-class").value;
+    var rank = parseInt(document.getElementById("rank").value);
+
+}
+
+
+function displayFighter(fighter) {
+}
+
